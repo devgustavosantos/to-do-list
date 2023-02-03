8cd64c6585b8d0d55c3f03ac4bfa7855ab3d72bd
Gustavo Santos <devgustavosantos@gmail.com>
2023-02-03 20:11:35 -0300

    feat: creates the Empty component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,12 @@
+import { Empty } from '../Empty';
 import { Input } from '../Input';
 import { Summary } from '../Summary';
 import { Task } from '../Task';
 import styles from './styles.module.css';
 
 export function Main() {
+  const tasks: string[] = ['1'];
+
   return (
     <main className={styles.main}>
       <Input />
@@ -12,14 +15,19 @@ export function Main() {
           total={2}
           completed={1}
         />
-        <Task
-          content="Uma tarefa legal"
-          status="completed"
-        />
-        <Task
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur architecto quod labore nihil atque nesciunt in consequuntur minima, culpa corporis maiores quis, temporibus velit qui tempore sint consequatur eveniet placeat."
-          status="in progress"
-        />
+        {tasks.length === 0 && <Empty />}
+        {tasks.length !== 0 && (
+          <>
+            <Task
+              content="Uma tarefa legal"
+              status="completed"
+            />
+            <Task
+              content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur architecto quod labore nihil atque nesciunt in consequuntur minima, culpa corporis maiores quis, temporibus velit qui tempore sint consequatur eveniet placeat."
+              status="in progress"
+            />
+          </>
+        )}
       </section>
     </main>
   );
